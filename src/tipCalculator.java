6e0960926188f8efd1a741d108eaa4179eda2999
chloed7 <114586399+chloed7@users.noreply.github.com>
2022-10-05 15:34:39 -0400

    Update tipCalculator.java

--- src/tipCalculator.java
+++ src/tipCalculator.java
@@ -8,27 +8,20 @@ public class tipCalculator {
         double bill = scan.nextDouble();
         System.out.println("Please enter the tip percentage (without the percentage sign): ");
         double tip = scan.nextInt();
+        tip /= 100.0;
         System.out.println("Please enter the number of people in the party: ");
         int people = scan.nextInt();
 
-        tip /= 100.0;
-        double tipPer = (bill * tip)/people;
-        double billPer = (bill/people);
-        double totalPer = (billPer + tipPer);
-        double billTotal = (tipPer * people) + bill;
-        double tipTotal = (tipPer * people);
+        double tipPer = Math.round(((bill * tip)/people)*100.0)/100.0;
+        double billPer = Math.round((bill/people)*100.0)/100.0;
+        double totalPer = Math.round((billPer + tipPer)*100.0)/100.0;
+        double billTotal = Math.round(((tipPer * people) + bill)*100.0)/100.0;
+        double tipTotal = Math.round((tipPer * people)*100.0)/100.0;
 
-        System.out.println(tip);
 
         System.out.println("The total tip amount is $" + tipTotal);
         System.out.println("The total bill including tip is $" + billTotal);
         System.out.println("The tip per person is $" + tipPer);
         System.out.println("The total per person is $" + totalPer);
-
-
-
-
-
-
     }
 }
